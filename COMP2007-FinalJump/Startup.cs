5733d4f8626e1302cd8f2445726b86ddc2639833
KarenSpringford <jkspringford@gmail.com>
2016-08-04 14:30:29 -0400

    Add project files.

---
+++ COMP2007-FinalJump/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2007_FinalJump.Startup))]
+namespace COMP2007_FinalJump
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
